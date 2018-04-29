0591bc04d25eda2f82b5ea3f864a2a87114be40f
my <my@192.168.1.2>
2018-04-29 19:21:21 +0530

    Synochrone Intvw Coding question

---
+++ JavaPractices2015/src/programing/practice/sep2017/SynechronIntrvwExmp.java
@@ -0,0 +1,29 @@
+package programing.practice.sep2017;
+
+public class SynechronIntrvwExmp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arraInt = {0,0,1,0,0,1,0,1,0};
+		int count=0;
+		for(int i=0; i<arraInt.length; i++){
+			int j=arraInt[i];
+			
+			if(equalToZero(j))
+				count++;
+		}
+		System.out.println("Count :"+count);
+	}
+
+	private static boolean equalToZero(int j) {
+		// TODO Auto-generated method stub
+		try{
+		int val = 1/j;
+			return false;
+		}catch(Exception e){
+			return true;
+		}
+		
+	}
+	
+}
